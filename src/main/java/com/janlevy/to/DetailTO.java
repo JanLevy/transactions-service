6c37946b0781c167f5205ab3946da12f4c001797
Jan Levý <hayashiminoru@gmail.com>
2025-09-05 10:16:16 +0200

    Odebrani prebytecneho importu

--- src/main/java/com/janlevy/to/DetailTO.java
+++ src/main/java/com/janlevy/to/DetailTO.java
@@ -1,8 +1,6 @@
 package com.janlevy.to;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record DetailTO(String detail1, String detail2, String detail3, String detail4) {
 }
